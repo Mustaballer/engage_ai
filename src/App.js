02e42016a339aa7effbbf2913c36fc1b545b2062
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-01-15 08:06:48 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,90 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React, {useEffect, useState} from 'react';
+import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
 import {sendFullTranscript, connectToServer} from './api';
+import './App.css';
 
 const App = () => {
- const [message, setMessage] = useState('');
- const [disabled, setDisabled] = useState(true);
+    const [message, setMessage] = useState('');
+    const [disabled, setDisabled] = useState(true);
 
- const commands = [
-  {
-    command: 'reset',
-    callback: () => resetTranscript()
-  },
-  {
-    command: 'shut up',
-    callback: () => setMessage('I wasn\'t talking.')
-  },
-  {
-    command: 'hello',
-    callback: () => setMessage('Hi there!')
-  },
-]
+    const commands = [
+        {
+            command: 'reset',
+            callback: () => resetTranscript()
+        },
+        {
+            command: 'shut up',
+            callback: () => setMessage('I wasn\'t talking.')
+        },
+        {
+            command: 'hello',
+            callback: () => setMessage('Hi there!')
+        },
+    ]
 
- const {
-  transcript,
-  interimTranscript,
-  finalTranscript,
-  resetTranscript,
-  listening,
-} = useSpeechRecognition({ commands });
+    const {
+        transcript,
+        interimTranscript,
+        finalTranscript,
+        resetTranscript,
+        listening,
+    } = useSpeechRecognition({commands});
 
- function sendTranscript() {
-    SpeechRecognition.stopListening();
-    sendFullTranscript(finalTranscript);
-  }
+    function sendTranscript() {
+        SpeechRecognition.stopListening();
+        sendFullTranscript(finalTranscript);
+    }
 
- function resetScript() {
-    resetTranscript();
-    setDisabled(true);
- }
+    function resetScript() {
+        resetTranscript();
+        setDisabled(true);
+    }
 
- useEffect(() => {
-   if (finalTranscript !== '') {
-     console.log('Got final result:', finalTranscript);
-   }
- }, [interimTranscript, finalTranscript]);
- if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-   return null;
- }
+    useEffect(() => {
+        if (finalTranscript !== '') {
+            console.log('Got final result:', finalTranscript);
+        }
+    }, [interimTranscript, finalTranscript]);
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        return null;
+    }
 
- if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-   console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
- }
- const listenContinuously = () => {
-  if(disabled) {
-    SpeechRecognition.startListening({
-      continuous: true,
-      language: 'en-US',
-    });
-  }
- };
- return (
-   <div>
-     <div>
-       <span>
-         listening:
-         {' '}
-         {listening ? 'on' : 'off'}
-       </span>
-       <div>
-         <button type="button" disabled={disabled} onClick={resetScript}>Reset</button>
-         <button type="button" onClick={() => {
-             setDisabled(false);
-             connectToServer();
-             listenContinuously();
-         }}>Start Session</button>
-         <button type="button" disabled={disabled} onClick={sendTranscript}>Stop</button>
-       </div>
-     </div>
-     <div>
-       {message}
-     </div>
-     <div>
-       <span>{transcript}</span>
-     </div>
-   </div>
- );
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
+    }
+    const listenContinuously = () => {
+        if (disabled) {
+            SpeechRecognition.startListening({
+                continuous: true,
+                language: 'en-US',
+            });
+        }
+    };
+    return (
+        <div className="black-background">
+            <div>
+                <div>
+                    <h1 className="title">Engage AI</h1>
+                    <div className="center">
+                        <button className="button" type="button" disabled={disabled} onClick={resetScript}>Reset
+                        </button>
+                        <button className="button" type="button" onClick={() => {
+                            setDisabled(false);
+                            connectToServer();
+                            listenContinuously();
+                        }}>Start Session
+                        </button>
+                        <button className="button" type="button" disabled={disabled} onClick={sendTranscript}>Stop
+                        </button>
+                    </div>
+                    <div className="centerText">
+                        <span className="status">Listening: {listening ? 'on' : 'off'}</span>
+                    </div>
+                </div>
+            </div>
+            <div>
+                {message}
+            </div>
+            <div className="black-text">
+                <textarea
+                    style={{
+                        width: '80%',
+                        height: '200px',
+                        fontSize: '20px',
+                        padding: '10px',
+                        backgroundColor: 'white',
+                        color: 'black',
+                        zIndex: 1
+                    }}
+                    value={transcript}
+                >
+      </textarea>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
 import MultipleChoice from './components/MultipleChoice.js';
 
 const Routing = () => {
-  return(
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<App/>} />
-        <Route path='/student' element={<MultipleChoice/>} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <div style={{backgroundColor: 'black', height: '100vh'}}>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<App/>}/>
+                    <Route path='/student' element={<MultipleChoice/>}/>
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Routing />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Routing/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
